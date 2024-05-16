29031d8cb5b4ab0fa5e9d4d93812d172bd51a522
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-16 20:22:59 +0530

    feat: complete all the required sockets

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -16,8 +16,42 @@ socketServer.on("connection", (socket) => {
     console.log(`Socket Connected `, socket.id)
     socket.on('room:join', (data) => {
         const { username, email, room } = data;
+        socketServer.to(room).emit("user:joined", {
+            username,
+            email,
+            id: socket.id
+        })
         mapUsernameWithSocketId.set(username, socket.id);
         mapSocketIdWithUsername.set(socket.id, username);
+        socket.join(room);
         socketServer.to(socket.id).emit('room:join', data);
+    })
+
+    socket.on('user:call', ({ to, offer }) => {
+        socketServer.to(to).emit('incomming:call', {
+            from: socket.id,
+            offer
+        });
+    });
+
+    socket.on('call:accepted', ({ to, ans }) => {
+        socketServer.to(to).emit('call:accepted', {
+            from: socket.id,
+            ans
+        });
+    });
+
+    socket.on('peer:negotiationneeded', ({ to, offer }) => {
+        socketServer.to(to).emit('peer:negotiationneeded', {
+            from: socket.id,
+            offer
+        });
+    })
+
+    socket.on('peer:negotiationdone', ({ to, ans }) => {
+        socketServer.to(to).emit('peer:negotiationfinal', {
+            from: socket.id,
+            ans
+        });
     })
 })
