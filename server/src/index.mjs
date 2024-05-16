a43d4925dfbb620013b59c296652e8b26261e536
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-05-16 17:48:34 +0530

    feat: add new event room:join on socket

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -5,8 +5,19 @@ dotenv.config();
 
 const PORT = process.env.PORT || 7000;
 
-const socket = new Server(PORT);
+const socketServer = new Server(PORT, {
+    cors: true,
+});
 
-socket.on("connection", (socket) => {
+const mapUsernameWithSocketId = new Map();
+const mapSocketIdWithUsername = new Map();
+
+socketServer.on("connection", (socket) => {
     console.log(`Socket Connected `, socket.id)
+    socket.on('room:join', (data) => {
+        const { username, email, room } = data;
+        mapUsernameWithSocketId.set(username, socket.id);
+        mapSocketIdWithUsername.set(socket.id, username);
+        socketServer.to(socket.id).emit('room:join', data);
+    })
 })
